1d0cba0298a7033cb9e0a39452d0596b10877bfc
Alex Hapgood <ach1@vt.edu>
2016-10-30 23:58:58 -0400

    final commit
    out

---
+++ out/webhelp-responsive/context-help-map.js
@@ -0,0 +1,1 @@
+var contextIds = {"topic-breakfast-recipes":"topic.html","t-vfriedck":"vchicken_ingredients.html","t-vfriedck-cooking":"vchicken_cook.html","t-vwaffles":"vwaffle_ingredients.html","t-vwaffles-cooking":"vwaffles_cook.html"}
---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,8 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "topic.html";
+fl["1"]= "vchicken_cook.html";
+fl["2"]= "vchicken_ingredients.html";
+fl["3"]= "vwaffle_ingredients.html";
+fl["4"]= "vwaffles_cook.html";
+var doStem = false;searchLoaded = true;
